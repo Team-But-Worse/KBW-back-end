c1d6e24c56fc107983b0c993a8decb24ec060315
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-10-10 16:25:00 +0000

    URL CORS change

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 const server = http.createServer(app);
 const socketServer = io(server, {
   cors: {
-    origin: "https://6343a6a00602f628fea36e06--kbw.netlify.app",
+    origin: "https://kbw.netlify.app",
     methods:["GET", "POST"]
   }
 })
